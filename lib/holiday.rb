b40e3917a3523951bd2f69414f4405805c298cb8
Eric Manning <emanningmusic@yahoo.com>
2019-10-28 18:45:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
    holiday_hash.each do |season, holiday|
    puts "#{season.capitalize}:"
    holidays.each do |holiday, supplies|
-     puts " #{holiday.to_s.split}: "
+     puts " #{holiday.to_s.split("_")}: "
    
    end
   # iterate through holiday_hash and print items such that your readout resembles:
