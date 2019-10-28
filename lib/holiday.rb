de2cbd97f59919c7f4eb8ff1e490ddc45d815013
Eric Manning <emanningmusic@yahoo.com>
2019-10-28 18:49:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
    puts "#{season.capitalize}:"
    holidays.each do |holiday, supplies|
      puts " #{holiday.to_s.split("_").map{|w|
-     w.capitalize}.join(" ")}:"
+     w.capitalize}.join(" ")}: #{supplies"
    end
    
    end
